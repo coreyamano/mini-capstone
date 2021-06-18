9199a1d401d56a304f90a243b9633f94128cda4f
Corey <corey.amano@gmail.com>
2021-06-17 19:09:13 -0700

    add patch route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,17 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def update
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    product.update(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_url],
+      description: params[:input_description],
+    )
+    product.save
+    render json: product.as_json
+  end
 end

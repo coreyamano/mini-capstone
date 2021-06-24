a0b7bfb218db277b30cc959e5210b166164067a6
Corey <corey.amano@gmail.com>
2021-06-24 16:21:35 -0700

    add supplier and products methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,10 @@ class Product < ApplicationRecord
   validates :inventory, numericality: { only_integer: true }
   validates :description, length: { in: 10..500 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
   def is_discounted?
     if price < 10
       true
